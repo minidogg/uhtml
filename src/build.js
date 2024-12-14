80c683db46f87b5f95f57d65cb3c1a2df48089b8
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-12-13 21:58:07 -0600

    i keep forgetting to commit

--- src/build.js
+++ src/build.js
@@ -3,8 +3,18 @@ import fs, { stat } from 'fs'
 
 import { XMLParser, XMLBuilder, XMLValidator} from "fast-xml-parser"
 
+
 export function BuildApp(appPath, buildPath){
     console.log("Beginning build")
+
+    const componentCache = {
+        "PLACEHOLDER": [
+            {
+                "props": {test: "abcd"},
+                "result": "xyz"
+            }
+        ]
+    }
 
     const pagePath = path.join(appPath, "pages")
     const staticPath = path.join(appPath, "static")
@@ -22,11 +32,35 @@ export function BuildApp(appPath, buildPath){
         let resolvedPagePath = path.join(pagePath, filePath)
         let resolvedBuildPath = path.join(buildPath, filePath.replace(/(?=.*).uhtml$/, ".html"))
 
-        let builtComponent = BuildComponent(fs.readFileSync(resolvedPagePath, "utf-8"))
+        let builtComponent = BuildComponent(fs.readFileSync(resolvedPagePath, "utf-8"), componentCache);
+
         fs.writeFileSync(resolvedBuildPath, builtComponent, "utf-8")
     })
 }
 
-function BuildComponent(componentText){
-    return " "
+function BuildComponent(componentText, componentCache){
+    const parser = new XMLParser({
+        ignoreAttributes: false,
+        attributeNamePrefix : "@_"
+    });
+    let jObj = parser.parse(componentText);
+
+    let imports = []
+    for(let key of Object.keys(jObj)){
+        if(key!="import")continue;
+        let importItem = {
+            from: path.resolve(jObj[key]["@_from"]),
+            as: jObj[key]["@_as"],
+        }
+        delete jObj[key]
+
+        imports.push(importItem)
+    }
+
+    const builder = new XMLBuilder({
+        ignoreAttributes: false
+    });
+    const xmlContent = builder.build(jObj);
+
+    return xmlContent;
 }
